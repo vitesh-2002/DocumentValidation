3a6a89934eb6ab48ea9c29c5006968fee997a167
Vitesh <73609570+vitesh-2002@users.noreply.github.com>
2022-09-30 17:07:54 -0400

    Add files via upload

--- extract_json.py
+++ extract_json.py
@@ -1,4 +1,35 @@
+
 import json
+
+# print your json data from the file
+with open('result.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
+# print(json_data)
+
+# Extract specific data from JSON file
+text = ''
+data = (json_data['document']['page']['row'])
+for x in range(len(data)):
+    subj = data[x]['column']
+    for s in subj:
+        if(len(s['text']) > 1):
+
+            print(s['text']['#text'])
+            text += s['text']['#text']
+
+# name
+# year
+# employer name
+
+
+
+
+
+
+
+
+
 
 '''
 json_data = '{"python": 1, "php": 2, "c": 3, "vb": 4, "perl": 5}'
@@ -11,26 +42,11 @@ print(json.dumps(json_load, indent=4))
 for x in json_load:
     print("%s: %d" % (x, json_load[x]))
 '''
-# print your json data from the file
-with open('result.json', 'r') as json_file:
-    json_data = json.load(json_file)
 
-# print(json_data)
 
-# Extract specific data from JSON file
 '''data = (json_data['document']['page']['row'][0]['column'])
 print(data)
 print(data[0])
 for x in data:
     if (len(x['text']) > 1):
         print(x['text']['#text'])'''
-text = ''
-data = (json_data['document']['page']['row'])
-for x in range(len(data)):
-    subj = data[x]['column']
-    for s in subj:
-        if(len(s['text']) > 1):
-
-            print(s['text']['#text'])
-            text += s['text']['#text']
-
